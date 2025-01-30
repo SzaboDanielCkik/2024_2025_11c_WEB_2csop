2070436c51f66c676025cb8df0f1bee75b9e63d7
Szabó Dániel <szabo.daniel@ckik.loc>
2025-01-30 12:24:43 +0100

    2025_01_30

---
+++ 2025_01_30 Javascript alapjai/script.js
@@ -0,0 +1,21 @@
+console.log("kapcsolat");
+
+var a = 13;
+var s = "13";
+var sz = '15';
+var b = true;
+
+console.log(a);
+console.log(s);
+console.log(sz);
+console.log(b);
+
+// matematikai operátorok: +-*/%
+// logikai operátorok: && || 
+// bitenkénti operátorok: & | ^
+// szöveges operátor: konkatenálás-összefűzés +
+
+console.log(s+b);
+console.log(a+b); //true 1, false 0
+console.log(a === s); // == értékvizsgálat, === érték+típus vizsgálat
+
