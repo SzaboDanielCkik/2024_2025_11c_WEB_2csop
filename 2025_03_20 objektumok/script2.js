9ffce866579ea4000028dcd921720863b9949547
Szabó Dániel <szabo.daniel@ckik.loc>
2025-03-31 08:35:54 +0200

    2025_03_31

--- 2025_03_20 objektumok/script2.js
+++ 2025_03_20 objektumok/script2.js
@@ -39,8 +39,33 @@ function feladat2(adatok){
     console.log(`A helyhatósági választáson ${adatok.length} képviselőjelölt indult.`);
 }
 
+function VanEAdottNevIndex(adatok,vnev, knev){
+    let i = 0;
+    while(i<adatok.length && !(adatok[i].veznev===vnev && adatok[i].kernev===knev)){
+        i++;
+    }
+    if(i>=adatok.length){
+        return -1;
+    }
+    else{
+        return i;
+    }
+}
+
 function feladat3(adatok){
-    
+    // const nev = "Ablak Antal";
+    const vnev = "Ablak";
+    const knev = "Antal";
+    const index = VanEAdottNevIndex(adatok,vnev, knev);
+    if(index>=0){
+        const adat = adatok[index];
+        console.log(`${adat.veznev} ${adat.kernev} - ${adat.szavazatok} szavazatot kapott!`);
+    }
+    else{
+        console.log("Ilyen nevű képviselőjelölt nem szerepela nyilvántartásban!");
+    }
+
+
 }
 
 function main(szoveg){
