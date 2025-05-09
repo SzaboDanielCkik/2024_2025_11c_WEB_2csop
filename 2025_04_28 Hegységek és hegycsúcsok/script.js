121aa9e84800cf5b62a4d95cb3509d0391a27e56
Dániel Szabó <szabo.daniel@ckik.hu>
2025-05-09 08:30:02 +0200

    2025_05_09

---
+++ 2025_04_28 Hegységek és hegycsúcsok/script.js
@@ -0,0 +1,46 @@
+
+function elsoFeladatEllenorzes(){
+    const joValasz = document.getElementById("börzsöny").checked;
+    if(joValasz)
+        return 2;
+    const rosszValasz1 = document.getElementById("badacsony").checked;
+    const rosszValasz2 = document.getElementById("bakony").checked;
+    const rosszValasz3 = document.getElementById("bükk").checked;
+    if(rosszValasz1 || rosszValasz2 || rosszValasz3)
+        return -1;
+
+    return 0;
+}
+
+function harmadikFeladatEllenorzes(){
+    let valaszok = document.getElementsByName("harmadik");
+    let jopont = 0;
+    let rosszpont = 0;
+    for(let i = 0; i<valaszok.length; i++){
+        if(valaszok[i].checked && (valaszok[i].id ==="budaihegyseg" || valaszok[i].id==="keszthelyihegyseg" || valaszok[i].id === "pilis" || valaszok[i].id === "vertes")){
+            jopont++;
+        }
+        else if(valaszok[i].checked){
+            rosszpont--;
+        }
+    }
+    if(rosszpont<0){
+        if(jopont+rosszpont<0)
+            return 0;
+        else 
+            return jopont+rosszpont;
+    }
+    else if(rosszpont === 0 && jopont === 4)
+        return 8;
+    else
+        return jopont;
+
+}
+
+
+function kiertekeles(){
+    let pont = 0;
+    pont += elsoFeladatEllenorzes(); 
+    pont += harmadikFeladatEllenorzes();
+    console.log(pont);   
+}
