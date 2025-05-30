a93b92c4975b90d50c6cca9e6af6eeb8c747d4eb
Dániel Szabó <szabo.daniel@ckik.hu>
2025-05-30 07:55:53 +0200

    Új elemek létrehozása

--- 2025_05_26 Gyakorlás/script.js
+++ 2025_05_26 Gyakorlás/script.js
@@ -1,3 +1,23 @@
+let index = 1;
+let utolso = 4;
+
+function Elozore(){
+    index--;
+    if(index == 0)
+        index = utolso;
+    const kep = document.getElementById("kep");
+    kep.setAttribute("src","kepek/image-"+index+".jpg");
+}
+
+function Kovetkezore(){
+    index++;
+    if(index == utolso + 1)
+        index = 1;
+    const kep = document.getElementById("kep");
+    kep.setAttribute("src","kepek/image-"+index+".jpg");
+
+}
+
 function random(a,b){
     return Math.floor(Math.random()*(b-a+1)+a);
 }
@@ -6,12 +26,16 @@ function kockaGeneralas(){
     const kockaDiv = document.getElementById("kockaDiv");
     const dbInput = document.getElementById("dbInput").value;
     kockaDiv.innerHTML="";
+    const osszegLabel = document.getElementById("osszegLabel");
+    let osszeg = 0;
 
     for(let i = 0; i<dbInput; i++)
     {
         let ujKep = document.createElement("img");
         const rand = random(1,6);
+        osszeg += rand;
         ujKep.setAttribute("src","kockak/Kocka"+rand+".png");
         kockaDiv.appendChild(ujKep);
     }
+    osszegLabel.innerHTML = "Összeg: "+osszeg;
 }
---
+++ 2025_05_30 Tablageneralas/script.js
@@ -0,0 +1,12 @@
+
+function Generalas(){
+    const tablaDiv = document.getElementById("tablaDiv");
+    const sor = parseInt(document.getElementById("sorInput").value);
+    const oszlop = parseInt(document.getElementById("oszlopInput").value);
+    //console.log(sor+" "+ oszlop);
+
+    let tabla = document.createElement("table");
+    let tr = document.createElement("tr");
+    let td = document.createElement("td");
+
+}
